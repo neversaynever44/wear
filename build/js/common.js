06c138d2d65c60a5541f7c5176766b8534cc64b2
neversaynever44 <killy-08>
2017-04-26 11:52:08 +0300

    add icons for all pages AR

--- build/js/common.js
+++ build/js/common.js
@@ -1,3 +1,87 @@
+
+$(function() {
+
+  $('form').each(function(){
+    // Объявляем переменные (форма и кнопка отправки)
+  var form = $(this),
+        btn = form.find('#form-submit');
+
+    // Добавляем каждому проверяемому полю, указание что поле пустое
+  form.find('.rfield').addClass('empty_field');
+
+    // Функция проверки полей формы
+    function checkInput(){
+      form.find('.rfield').each(function(){
+        if($(this).val() != ''){
+          // Если поле не пустое удаляем класс-указание
+    $(this).removeClass('empty_field');
+        } else {
+          // Если поле пустое добавляем класс-указание
+    $(this).addClass('empty_field');
+        }
+      });
+    }
+
+    // Функция подсветки незаполненных полей
+    function lightEmpty(){
+      form.find('.empty_field').css({'background-color':'red'});
+      // Через полсекунды удаляем подсветку
+      setTimeout(function(){
+        form.find('.empty_field').removeAttr('style');
+      },1500);
+    }
+
+    // Проверка в режиме реального времени
+    setInterval(function(){
+      // Запускаем функцию проверки полей на заполненность
+    checkInput();
+      // Считаем к-во незаполненных полей
+      var sizeEmpty = form.find('.empty_field').size();
+      // Вешаем условие-тригер на кнопку отправки формы
+      if(sizeEmpty > 0){
+        if(btn.hasClass('disabled')){
+          return false
+        } else {
+          btn.addClass('disabled')
+        }
+      } else {
+        btn.removeClass('disabled')
+      }
+    },500);
+
+    // Событие клика по кнопке отправить
+    btn.click(function(){
+      if($(this).hasClass('disabled')){
+        // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
+    lightEmpty();
+        return false
+      } else {
+        // Все хорошо, все заполнено, отправляем форму
+        // form.submit();
+        var plane =      $('#flyPlane');
+		var btnSubmit   = $('#form-submit')
+		$('.form-label').on("click",function(){
+			   		 btnSubmit.prop("disabled", true);
+				    plane.addClass('is-active');
+				    btnSubmit.addClass('is-hidden');
+			      setTimeout(function(){
+			       plane.removeClass('is-active'); 
+			      btnSubmit.removeClass('is-hidden'); 
+			      	   btnSubmit.removeAttr('disabled');
+			  	      	document.getElementById('form').submit();
+			   		},1300);
+
+		})
+
+
+
+
+      }
+    });
+  });
+});
+
+
 // $(document).ready(function(){
 
 // var client = document.querySelector('.menu').clientHeight;
@@ -77,20 +161,23 @@ $('.js-close-popup').click(function (e) {
 })
 
 // Animation button-send
-var plane = $('#flyPlane');
-$('.form-label').on("click",function(){
-   if(!plane.hasClass('is-active')){
-	    plane.addClass('is-active');
-	    $('#form-submit').addClass('is-hidden');
-	    $('#form-submit').attr('disabled', 'disabled');
-      setTimeout(function(){
-       plane.removeClass('is-active'); 
-       $('#form-submit').removeClass('is-hidden'); 
-      	   $('#form-submit').removeAttr('disabled');
-  	      	document.getElementById('form').submit();
-   		},1300);
- 	 }
-})
+// var plane = $('#flyPlane');
+// var btnSubmit   = $('#form-submit')
+// $('.form-label').on("click",function(){
+// 	if(!$('.input').hasClass('empty_field')){
+// 	   if(!plane.hasClass('is-active')){
+// 	   		 btnSubmit.attr('disabled', 'disabled');
+// 		    plane.addClass('is-active');
+// 		    btnSubmit.addClass('is-hidden');
+// 	      setTimeout(function(){
+// 	       plane.removeClass('is-active'); 
+// 	      btnSubmit.removeClass('is-hidden'); 
+// 	      	   btnSubmit.removeAttr('disabled');
+// 	  	      	document.getElementById('form').submit();
+// 	   		},1300);
+// 	 	 }
+//  	}
+// })
 
 // anchor
 $('.arrow-up').on('click', function() {
@@ -261,3 +348,5 @@ function getScrollWidth() {
 
 	return scrollWidth;
 };
+
+
